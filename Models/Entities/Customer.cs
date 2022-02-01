5bc64adefba725c80b58c45888fb35ab9fa7759f
Thanapon Chapchainai <tnc@ii.co.th>
2022-02-02 00:12:05 +0700

    entities

---
+++ Models/Entities/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IIGApi.Models.Entities
+{
+    public class Customer
+    {
+        public long Id { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public string? Email { get; set; }
+        public string? MobilePhone { get; set; }
+        public int? Age { get; set; }
+    }
+}
---
+++ Models/Entities/iiGContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace IIGApi.Models.Entities
+{
+    public partial class iiGContext : DbContext
+    {
+        public iiGContext(DbContextOptions<iiGContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+    }
+}
